9fbf3c9dffb73a8e74090396673a7460959b0b10
sm14 <sm14@linux1.gl.umbc.edu>
2018-04-02 14:24:07 -0400

    queries and stuff

--- index.php
+++ index.php
@@ -1,50 +1,43 @@
-<!DOCTYPE html>
-<html>
-
 <?php
-	echo "test?";
+	//echo "test?";
+	//print_r($_POST);
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		 $name = $_POST["user_name"];
+ 		 $pass = $_POST["password"];
+   	}	
+
+	//echo $name;
+	//echo $pass;
 
 	$user = 'snturskey';
 	$password = 'npnp';
 	$server_name = 'studentdb-maria.gl';
 	 	
-	$conn = mysqli_connect($server_name, $user, $password);
-	
+	$conn = mysql_connect($server_name, $user, $password);
+	$db = mysql_select_db('snturskey',$conn);	
+
 	if (!$conn) {
 		die("Connection failed: " . mysqli_connect_error());
 	}
 	echo "Connected successfully";
+
+	$query = "SELECT first_name FROM users WHERE user_name='kdillon' AND password='pass'";
+	$result = mysql_query($query) or die('Error querying database.');
+
+	echo $result; 
+
+	// query will only return first name
+	while ($row = mysql_fetch_assoc($result)) {
+		print_r($row);
+
+		if ($name == $row['first_name']) {
+			header("Location:home.html");
+		} else {
+			echo "no";
+		}	
+	}
+
+
 ?>
 
-
-<head>
-	<link rel="stylesheet" type="text/css" href="styles.css">
-
-	<script>
-		function login() {
-			var user_name = document.getElementById("user_name");
-			var password = document.getElementById("password");
-			//alert("Logging in..."); 
-			window.location.href = 'home.html';
-		}
-		function noAccount() {
-			alert("That sucks");
-		}
-	</script>
-	
-</head>
-
-<body style="text-align:center">
-	<div id="loginBox">
-		<h1>Welcome to the UMBC Facilities MAPS</h1>
-		<span style="padding:5px;">
-			User Name: <input type="text" id="user_name"></input></br></br>
-			Password: <input type="text" id="password"></input></br></br>
-			<button class="redirectButton" onclick="login()">Login</button>
-		</span>
-	</div>
-
-	<p onclick="noAccount()">Don't already have an account?</p>
-</body>
-
-</html>
