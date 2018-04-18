c0c4656178c5e2688406b7ad5b3445dd2219c2fc
sm14 <sm14@linux3.gl.umbc.edu>
2018-04-18 14:00:06 -0400

    kind of login

--- php/login.php
+++ php/login.php
@@ -1,10 +1,10 @@
-// PHP script for the  login() function
-// This function will take a username and a password. It will check the database for that user
-// and password and return the type of account that is being logged into so that the website can
-// direct the user to the right landing page. If the user does not exist in the table, it will
-// return an error.
-
 <?php
+
+	// PHP script for the  login() function
+	// This function will take a username and a password. It will check the database for that user
+	// and password and return the type of account that is being logged into so that the website can
+	// direct the user to the right landing page. If the user does not exist in the table, it will
+	// return an error.
 
 	//echo "test?";
 	//print_r($_POST);
@@ -15,8 +15,8 @@
  		 $pass = $_POST["password"];
    	}	
 
-	//echo $name;
-	//echo $pass;
+	echo $name;
+	echo $pass;
 
 	// Credentials for accessing the database
 	$user = 'snturskey';
@@ -33,25 +33,31 @@
 	echo "Connected successfully";
 
 	// Query and the results. Rows are stored in the result variable
-	$query = "SELECT first_name FROM users WHERE user_name='kdillon' AND password='pass'";
+	//$query = "SELECT first_name FROM users WHERE user_name='kdillon' AND password='pass'";
+	$query = "SELECT user_name FROM users WHERE user_name='$name' AND password='$pass'";
 	$result = mysql_query($query) or die('Error querying database.');
 
 	echo $result; 
 	
 	// Loop over the returned rows and access information by attribute if desired
 	// query will only return first name
-	while ($row = mysql_fetch_assoc($result)) {
-		/*print_r($row);
+	/*while ($row = mysql_fetch_assoc($result)) {
+		print_r($row);
 
-		if ($name == $row['first_name']) {
-			header("Location:home.html");
+		if ($name == $row['user_name']) {
+			header("Location:../home.html");
 		} else {
 			echo "no";
-		}*/
+		}
 
-		header("Location:home.html");
+		//header("Location:home.html");
+	}*/
+
+	if ( ! mysql_num_rows($result) == 0 ) {	
+		header("Location:../home.html");	
+	} else {	
+		header("Location:../index.html");
 	}
-
 
 ?>
 
