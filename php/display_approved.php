e3a2926464a4541d865457c8540adbcdf01e9081
Katie <kat42@umbc.edu>
2018-04-22 08:51:34 -0400

    adding functions

---
+++ php/display_approved.php
@@ -0,0 +1,38 @@
+<?php
+
+	// PHP script for the  display_approved() function
+	// This function will grab the approved work orders and display them in the
+	// proper place on the page. 
+
+	// Code to retrieve variables sent from the html
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		 $name = $_POST["user_name"];
+ 		 $pass = $_POST["password"];
+   	}	
+
+	// Credentials for accessing the database
+	$user = 'snturskey';
+	$password = 'npnp';
+	$server_name = 'studentdb-maria.gl';
+
+	// Connecting to the database
+	$conn = mysql_connect($server_name, $user, $password);
+	$db = mysql_select_db('snturskey',$conn);	
+
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+
+	// Query and the results. Rows are stored in the result variable
+	$query = "SELECT * FROM users";
+	$result = mysql_query($query) or die('Error querying database.');
+
+	// Loop over the returned rows and access information by attribute if desired
+	while ($row = mysql_fetch_assoc($result)) {
+		print_r($row);
+		print_r($row['first_name'])
+	}
+
+
+?>
+
