a90ea1a9e9e562c30d932a7f5e37f72e7722f441
sm14 <sm14@linux1.gl.umbc.edu>
2018-04-22 17:10:28 -0400

    stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
+function authenticate(googleUser) {
+	
+	console.log("authenticate");
+	var profile = googleUser.getBasicProfile();
+	var email = profile.getEmail();
+	var token = googleUser.getAuthResponse().id_token;
+	console.log(token);
+}
+function onSignIn(googleUser) {
+
+	console.log("signIn func");
+
+	var profile = googleUser.getBasicProfile();
+ 	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	console.log('Name: ' + profile.getName());
+	console.log('Image URL: ' + profile.getImageUrl());
+	console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+	
+	authenticate(googleUser);
+}
 function viewAccount() {
 	window.location.href='view_account.html';
 }
@@ -54,5 +74,4 @@ function togglePassword() {
 	} else {				
 		document.getElementById("changePassword").style.display = "block";
 	}
-}
-
+} 
