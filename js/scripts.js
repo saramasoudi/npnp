12989a72db4ac06e92825ec5228967acf57d8640
sm14 <sm14@linux2.gl.umbc.edu>
2018-04-25 16:08:54 -0400

    logout

--- js/scripts.js
+++ js/scripts.js
@@ -18,14 +18,21 @@ function onSignIn(googleUser) {
 			//window.location.href = 'MA_home.html';
 			document.getElementById("mainFrame").src = "MA_home.html";
 			document.getElementById("loginButton").style.display = "none";
+			document.getElementById("menu").style.display = "flex";
 
 		} else if (auth.trim() == 'OA' || auth.trim() == 'SA') {
 			//window.location.href = 'home.html';
 			document.getElementById("mainFrame").src = "home.html";
 			document.getElementById("loginButton").style.display = "none";
+			document.getElementById("menu").style.display = "flex";
 	
 		} else {		
-			signOut();
+			
+			var auth2 = gapi.auth2.getAuthInstance();
+			auth2.signOut().then(function() {
+				console.log('User signed out.');
+			});
+
 			alert("User not authorized. Contact UMBC Facilities for access.");
 			//window.location.href = 'index.html';
 		}
@@ -37,6 +44,10 @@ function signOut() {
 	var auth2 = gapi.auth2.getAuthInstance();
 	auth2.signOut().then(function() {
 		console.log('User signed out.');
+
+		document.getElementById("mainFrame").src = "index.html";
+		document.getElementById("loginButton").style.display = "block";
+		document.getElementById("menu").style.display = "none";
 	});
 }
 
