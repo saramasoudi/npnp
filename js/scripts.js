6657278af8b21ee22aba829437a806a12de21103
saramasoudi <sm14@umbc.edu>
2018-04-27 14:20:19 -0400

    Update scripts.js
    trying to make a smarter toggle function, comments for readability

--- js/scripts.js
+++ js/scripts.js
@@ -6,32 +6,41 @@ function onSignIn(googleUser) {
 	var email = profile.getEmail();
 	var token = googleUser.getAuthResponse().id_token;
 
+	// ajax request to send account information to authenticate.php script
+	// authenticate.php will verify if the user's email is in the users table
+	// if yes, their token will be added if not already present
+	//	and their account type is returned
+	// if no, they are not given access to the site
 	var xhr = new XMLHttpRequest();
 	xhr.open('POST', 'php/authenticate.php');
 	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 	xhr.onload = function() {
-		console.log('Signed in as: ' + xhr.responseText);
 		var auth = xhr.responseText;
 
 		if (auth.trim() == 'MA') {
+			
+			// "redirect" to MA_home 
 			document.getElementById("mainFrame").src = "MA_home.html";
+			
+			// hide login button and show menu
 			document.getElementById("loginButton").style.display = "none";
-
 			document.getElementById("menu").style.display = "flex";		
 
 		} else if (auth.trim() == 'OA' || auth.trim() == 'SA') {
+			
+			// "redirect" to home
 			document.getElementById("mainFrame").src = "home.html";
+			
+			// hide login button and show menu and upload buttons
 			document.getElementById("loginButton").style.display = "none";
-
 			document.getElementById("menu").style.display = "flex";
 			document.getElementById("uploadButton").style.display = "block";		
 
 		} else {		
 			
+			// sign user out of google 
 			var auth2 = gapi.auth2.getAuthInstance();
-			auth2.signOut().then(function() {
-				console.log('User signed out.');
-			});
+			auth2.signOut().then(function() { });
 
 			alert("User not authorized. Contact UMBC Facilities for access.");
 		}
@@ -42,8 +51,14 @@ function onSignIn(googleUser) {
 function signOut() {
 	var auth2 = gapi.auth2.getAuthInstance();
 	auth2.signOut().then(function() {
+		
+		// "redirects" to login page 
 		document.getElementById("mainFrame").src = "index.html";
+		
+		// enable the login button 
 		document.getElementById("loginButton").style.display = "block";
+		
+		// hide buttons and menus 
 		document.getElementById("menu").style.display = "none";
 		document.getElementById("options").style.visibility = "hidden";
 		document.getElementById("uploadButton").style.display = "none";
@@ -52,27 +67,36 @@ function signOut() {
 
 function toggle(element) {
 
-	var id;
-	var _id;
+	var id = "options";
 
+	// upload button click
 	if (element == "uploadButton" || element == "exitUpload") {
 		id = "uploadForm";
+	// search menu option selected
 	} else if (element == "searchButton" || element == "exitSearch") {
 		id = "searchForm";
 		
-		// close menu when search overlay pops up
+		// close menu when overlay pops up
 		if (element == "searchButton") {
-			_id = "options";
+			toggleMenu();
 		}
-
+		
+	// accounts menu option selected
+	} else if (element == "accountsButton" || element == "exitAccounts") {
+		id = "accountsForm";
+		
+		// close menu when overlay pops up
+		if (element == "accountsButton") {
+			toggleMenu();
+		}
+		
+	// menu icon click
 	} else if (element == "menuToggle") {
-		id = "options";
-	}  
+		toggleMenu();
+	}
 
-	if (id == "options") {
-		toggleMenu();		
-	} else { 
-	
+	// toggle selected form and overlay 
+	if (id != "options") {		
 		if ( document.getElementById(id).style.display == "block" ) {
 			document.getElementById(id).style.display = "none";
 			document.getElementById("overlay").style.display = "none";			
@@ -80,15 +104,12 @@ function toggle(element) {
 			document.getElementById(id).style.display = "block";
 			document.getElementById("overlay").style.display = "block";
 		}
-
-		if (_id == "options") {
-			toggleMenu();
-		}
 	}
 }
 
+// menu uses visibility instead of display for style reasons 
 function toggleMenu() {  
-
+	
 	if ( document.getElementById("options").style.visibility == "visible" ) {
 		document.getElementById("options").style.visibility = "hidden";			
 	} else {
