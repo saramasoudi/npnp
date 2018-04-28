b05d8271b6c0bc6a126c63d4bbe9dcb914101cc8
Beatriz Tinoco <btinoco1@umbc.edu>
2018-04-28 12:34:07 -0400

    kinda wrote queries for searching

--- php/search.php
+++ php/search.php
@@ -3,13 +3,17 @@
 	// PHP script for the  search() function
 	// This function will take a variety of parameters and constraints and return the work orders that
 	// satisfy those constraints. Constraints include, work order status, upload date, completed date,
-	// and assigned maintenance assistant. 
+	// and assigned maintenance assistant.
 
 	// Code to retrieve variables sent from the html
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		 $name = $_POST["user_name"];
- 		 $pass = $_POST["password"];
-   	}	
+		 $unassigned = $_POST["unassigned"];
+ 		 $assigned = $_POST["assigned"];
+		 $submitted = $_POST["submitted"];
+ 		 $approved = $_POST["approved"];
+		 $ma_email = $_POST["ma_email"];
+		 $wo_id = $_POST["wo_id"];
+   	}
 
 	// Credentials for accessing the database
 	$user = 'snturskey';
@@ -18,16 +22,57 @@
 
 	// Connecting to the database
 	$conn = mysql_connect($server_name, $user, $password);
-	$db = mysql_select_db('snturskey',$conn);	
+	$db = mysql_select_db('snturskey',$conn);
 
 	if (!$conn) {
 		die("Connection failed: " . mysqli_connect_error());
 	}
 
-	// Query and the results. Rows are stored in the result variable
-	$query = "SELECT * FROM users";
-	$result = mysql_query($query) or die('Error querying database.');
+	// if no fields are selected
+	if ($unassigned == 'False' && $assigned == 'False' && $submitted == 'False' && $approved == 'False' && $ma_email == NULL && $wo_id == NULL) {
+		$query = "SELECT * FROM wo";
+		$result = mysql_query($query) or die('Error querying database.');
+	}
 
+	// if WO ID is selected
+	else if ($wo_id != NULL) {
+		$query = "SELECT * FROM wo WHERE order_id = $wo_id";
+		$result = mysql_query($query) or die('Error querying database.');
+	}
+
+	// check which other fields were selected
+	else {
+		if ($unassigned == 'True') {
+			$query1 = "SELECT * FROM wo WHERE status = 'unassigned'";
+			if ($ma_email != 	NULL) {
+				$query1 .= "AND assigned_ma = $ma_email"
+			}
+			$result1 = mysql_query($query1) or die('Error querying database.');
+		}
+		if ($assigned == 'True') {
+			$query2 = "SELECT * FROM wo WHERE status = 'assigned'";
+			if ($ma_email != 	NULL) {
+				$query2 .= "AND assigned_ma = $ma_email"
+			}
+			$result2 = mysql_query($query2) or die('Error querying database.');
+		}
+		if ($submitted == 'True') {
+			$query3 = "SELECT * FROM wo WHERE status = 'submitted'";
+			if ($ma_email != 	NULL) {
+				$query3 .= "AND assigned_ma = $ma_email"
+			}
+			$result3 = mysql_query($query3) or die('Error querying database.');
+		}
+		if ($approved == 'True') {
+			$query4 = "SELECT * FROM wo WHERE status = 'approved'";
+			if ($ma_email != 	NULL) {
+				$query4 .= "AND assigned_ma = $ma_email"
+			}
+			$result4 = mysql_query($query4) or die('Error querying database.');
+		}
+	}
+
+	
 	// Loop over the returned rows and access information by attribute if desired
 	while ($row = mysql_fetch_assoc($result)) {
 		print_r($row);
@@ -36,4 +81,3 @@
 
 
 ?>
-
