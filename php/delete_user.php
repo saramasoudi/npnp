0dda9b4e6606af7fa5c304ca61ac60cdd6a3987e
Katie <kat42@umbc.edu>
2018-05-03 23:41:00 -0400

    delete

--- php/delete_user.php
+++ php/delete_user.php
@@ -3,10 +3,7 @@
 	// PHP script for the delete_user() function
 	// This function will delete a user from the database.
 
-	// Code to retrieve variables sent from the html
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $ma_email = $_POST["ma_email"];
-  }
+	$ma_email = $_POST["ma_email"];
 
 	// Credentials for accessing the database
 	$user = 'snturskey';
@@ -21,21 +18,11 @@
 		die("Connection failed: " . mysqli_connect_error());
 	}
 
-	// Query and the results. Rows are stored in the result variable
-	// $query = "SELECT * FROM users";
-	// $result = mysql_query($query) or die('Error querying database.');
-
-	// Loop over the returned rows and access information by attribute if desired
-	// while ($row = mysql_fetch_assoc($result)) {
-	// 	print_r($row);
-	// 	print_r($row['first_name'])
-	//}
-
-  $query = "SELECT * FROM users WHERE email = '$ma_email'"
-  $result = mysql_query($query) or die('Error querying database.');
-  if (mysql_num_rows($result) == 0) {
-    echo "<script> console.log('You cannot delete an account that does not exist');</script>";
-  }
+	$query = "SELECT * FROM users WHERE email = '$ma_email'"
+  	$result = mysql_query($query) or die('Error querying database.');
+  	if (mysql_num_rows($result) == 0) {
+    	   echo "<script> console.log('You cannot delete an account that does not exist');</script>";
+  	}
 
 	$query1 = "DELETE FROM users WHERE email = '$ma_email'";
 	$result1 = mysql_query($query1) or die('Error querying database.');
