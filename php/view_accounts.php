ab5bae8dad5852e672ed86c281335887cb21de61
Katie <kat42@umbc.edu>
2018-05-03 23:06:59 -0400

    ikd

--- php/view_accounts.php
+++ php/view_accounts.php
@@ -22,6 +22,16 @@
 		die("Connection failed: " . mysqli_connect_error());
 	}
 
+	// Query and the results. Rows are stored in the result variable
+	// $query = "SELECT * FROM users";
+	// $result = mysql_query($query) or die('Error querying database.');
+
+	// Loop over the returned rows and access information by attribute if desired
+	// while ($row = mysql_fetch_assoc($result)) {
+	// 	print_r($row);
+	// 	print_r($row['first_name'])
+	//}
+
 	$query = "INSERT INTO users (email, account_type) VALUES ('$user_email', '$acct_type')";
 	$result = mysql_query($query) or die('Error querying database.');
 
