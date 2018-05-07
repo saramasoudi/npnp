211bb8e4eed0ef557bf71b6b45714a35a1df65d8
Katie <kat42@umbc.edu>
2018-05-06 21:03:17 -0400

    ?

--- js/scripts.js
+++ js/scripts.js
@@ -170,8 +170,8 @@ function showUsers() {
 
 function display() {
     var auth2 = gapi.auth2.getAuthInstance();
-    var user = auth2.currentUser.get().email;
-    console.log(auth2.currentUser.email);
+    var user = auth2.currentUser.get().getBasicProfile().email;
+    console.log(auth2.currentUser.get());
     console.log(user);
     
    //console.log("display");
